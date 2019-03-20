14b28cd3d2b38c0bbe3dec450c83eaf781ac58e0
jaejunmin <jaejunmi@usc.edu>
2019-03-19 19:01:16 -0700

    t

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
-// creating a connection to the websock server from browser
 
-
-// let connection = new WebSocket("ws://web-socket-node-jaejun.herokuapp.com");
-// var HOST = location.origin.replace(/^http/, 'ws')
-let connection = new WebSocket("//web-socket-node-jaejun.herokuapp.com");
+let connection = new WebSocket("wss://web-socket-node-jaejun.herokuapp.com");
 connection.onopen = () => {
   console.log('connected from the frotend');
-   connection.send('hello');
+   // connection.send('hello');
 };
 
 connection.onerror = () => {
